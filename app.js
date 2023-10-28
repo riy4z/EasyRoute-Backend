013d516ba2d1f7834ec83427087bda2ce644c972
Riyas Ahamed <56198819+riy4z@users.noreply.github.com>
2023-10-28 12:01:55 +0530

    Fetch AddressData Added

--- app.js
+++ app.js
@@ -32,27 +32,41 @@ const addressSchema = new mongoose.Schema({
     "longitude": Number,
     "latitude": Number,
   });
-  addressSchema.index({ "First Name": 1, "Last Name": 1 }, { unique: true });
+  addressSchema.index({ "First Name": 1, "Last Name": 1, "Street Address":1, "City": 1,"State":1 }, { unique: true });
   const AddressInfo = mongoose.model('AddressInfo', addressSchema);
 
-// Define a route to store addressData
+  // Define a route to store addressData
 app.post('/api/store-address-data', async (req, res) => {
-    try {
-      const data = req.body; 
-      // This assumes that your addressData is sent in the request body
-      const savedData = new AddressInfo(data);
-      await savedData.save();
-      res.status(201).json({ message: 'Address data saved successfully' });
-    } catch (error) {
-        if (error.code === 11000) {
-            // MongoDB error code 11000 indicates a duplicate key error
-            res.status(400).json({ error: 'Duplicate value found in unique field' });
-          } else {
-            res.status(500).json({ error: 'Error saving address data' });
-          }
-    }
-  });
+  try {
+    const data = req.body; 
+    // This assumes that your addressData is sent in the request body
+    const savedData = new AddressInfo(data);
+    await savedData.save();
+    res.status(201).json({ message: 'Address data saved successfully' });
+  } catch (error) {
+      if (error.code === 11000) {
+          // MongoDB error code 11000 indicates a duplicate key error
+          res.status(400).json({ error: 'Duplicate value found in unique field' });
+        } else {
+          res.status(500).json({ error: 'Error saving address data' });
+        }
+  }
+});
   
+// Define a route to retrieve addressData
+app.get('/api/get-address-data', async (req, res) => {
+  try {
+    const addressData = await AddressInfo.find({}); // Fetches all data from the AddressInfo collection
+    if (addressData && addressData.length > 0) {
+      res.setHeader('Content-Type', 'application/json'); 
+      res.status(200).json(addressData);
+    } else {
+      res.status(404).json({ message: 'No address data found' });
+    }
+  } catch (error) {
+    res.status(500).json({ error: 'Error retrieving address data' });
+  }
+});
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
