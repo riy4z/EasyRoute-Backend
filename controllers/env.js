64032ff4ad67d0585ca2e2c833904a5e8c70f1ff
Riyas Ahamed <56198819+riy4z@users.noreply.github.com>
2023-11-11 13:41:19 +0530

    SendInBlue (Brevo) API Integrated for Email

--- controllers/env.js
+++ controllers/env.js
@@ -1,3 +1,3 @@
 
 export const EMAIL = 'easyroutea2z@gmail.com';
-export const PASSWORD = 'ojuujqtquvpuqxrk';
+export const PASSWORD = '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';
--- controllers/mailer.js
+++ controllers/mailer.js
@@ -1,58 +1,56 @@
-import nodemailer from 'nodemailer';
-import Mailgen from 'mailgen';
-// import ENV from '../config.js';
-import { EMAIL, PASSWORD } from '../controllers/env.js';
+import { EMAIL, PASSWORD,  } from '../controllers/env.js';
+import brevo from '@getbrevo/brevo';
+let defaultClient = brevo.ApiClient.instance;
+
+let apiKey = defaultClient.authentications['api-key'];
+apiKey.apiKey = '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';
+
+let apiInstance = new brevo.TransactionalEmailsApi();
+let sendSmtpEmail = new brevo.SendSmtpEmail();
 
 
-let nodeConfig = {
-    service: "gmail",
-    auth: {
-    user: EMAIL,
-    pass: PASSWORD
- },
-}
-
-let transporter = nodemailer.createTransport(nodeConfig);
-
-let MailGenerator = new Mailgen({
-    theme:"default",
-    product:{
-        name:"EasyRoute",
-        link:"https://mailgen.js"
-    }
-})
-
-/** POST: localhost:4000/api/registerMail 
- * @param: {
-  "username" : "example123",
-  "userEmail" : "admin123",
-  "text" : "",
-  "subject" : ""
-}
-*/
 export const registerMail = async (req,res) => {
     const {username, userEmail, text, subject} = req.body;
 
-    var email = {
-        body: {
-            name : `${username}`,
-            intro: text || "Welcome to EasyRoute!",
-            outro: "Expecting you to visit here often"
-        }
-    }
+    sendSmtpEmail.subject =subject || "Registration Successful";
+    const htmlContent = text || `<html><body><h1>Welcome to EasyRoute, ${username}!</h1></body></html>`;
+    sendSmtpEmail.htmlContent = htmlContent;
+    sendSmtpEmail.sender = { "name": "EasyRoute", "email": "easyroutea2z@gmail.com" };
+    sendSmtpEmail.to = [
+        { "email": `${userEmail}`, "name": `${username}` }
+      ];
 
-    var emailBody = MailGenerator.generate(email);
-
-    let message = {
-        from : EMAIL,
-        to : userEmail,
-        subject : subject || "Signup Successful",
-        html : emailBody
-    }
-
-    transporter.sendMail(message)
-    .then(() => {
+    sendSmtpEmail.replyTo = { "email": "easyroutea2z@gmail.com", "name": "EasyRoute Support" };
+    sendSmtpEmail.headers = { "Some-Custom-Name": "unique-id-1234" };
+    sendSmtpEmail.params = { "parameter": "My param value", "subject": "common subject" };
+      
+      
+    apiInstance.sendTransacEmail(sendSmtpEmail).then(function (data) {
+        console.log('API called successfully. Returned data: ' + JSON.stringify(data));
         return res.status(200).send({msg : "You should receive an email from us."})
-    })
-    .catch(error => res.status(500).send({error}))
+      }, function (error) {
+        console.error(error);
+      });
+    // var email = {
+    //     body: {
+    //         name : `${username}`,
+    //         intro: text || "Welcome to EasyRoute!",
+    //         outro: "Expecting you to visit here often"
+    //     }
+    // }
+
+    // var emailBody = MailGenerator.generate(email);
+
+    // let message = {
+    //     from : "support@easyroute.com",
+    //     to : "jriyazamd@gmail.com",
+    //     subject : subject || "Signup Successful",
+    //     html : emailBody
+    // }
+
+    // transporter.sendMail(message)
+    // .then(() => {
+    //     return res.status(200).send({msg : "You should receive an email from us."})
+    // })
+    // .catch(error => res.status(500).send({error}))
 }
