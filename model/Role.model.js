2204d2e3fef3b247a6137ca0e78408e1bcc19c10
Shehnaz Rasheetha <shehnazrasheetha2@gmail.com>
2023-12-03 21:08:19 +0530

    Role Hierarchy and isadmin removed

--- model/Role.model.js
+++ model/Role.model.js
@@ -3,7 +3,8 @@ import mongoose from "mongoose";
 
 export const RoleSchema = new mongoose.Schema({
     Role : {type: String},
-    CompanyID: {type: ObjectId}
+    CompanyID: {type: ObjectId},
+    // RoleHierarchy:{type: Number}
 });
 
 export default mongoose.model.Roles || mongoose.model('Roles', RoleSchema);
