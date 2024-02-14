619fcdd4c1013c7c3073c6c2530a4542a433fc69
Shehnaz Rasheetha <shehnazrasheetha2@gmail.com>
2024-02-14 18:57:54 +0530

    Updated AddressInfo Model

--- model/AddressInfo.model.js
+++ model/AddressInfo.model.js
@@ -4,8 +4,8 @@ import { ObjectId } from "mongodb";
 export const addressSchema = new mongoose.Schema({
     "First Name": String,
     "Last Name": String,
-    "Phone": Number,
-    "Email": String,
+    "Phone": { type: Number, default: "" },
+    "Email": { type: String, default: "" },
     "Street Address": String,
     "City": String,
     "State": String,
@@ -13,10 +13,11 @@ export const addressSchema = new mongoose.Schema({
     "longitude": Number,
     "latitude": Number,
     "isHidden": { type: Boolean, default: false },
-    "CompanyID": {type: ObjectId},
-    "LocationID": {type: ObjectId},
-    "markerId": {type:String},
+    "CompanyID": { type: mongoose.Schema.Types.ObjectId },
+    "LocationID": { type: mongoose.Schema.Types.ObjectId },
+    "markerId": { type: String },
 });
+
 
 addressSchema.index({ "First Name": 1, "Last Name": 1, "Street Address":1, "City": 1,"State":1, "ZIP Code": 1, "LocationID": 1, "Phone":1,"Email":1}, { unique: true });
 
