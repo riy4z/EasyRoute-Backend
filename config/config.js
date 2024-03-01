dce378d51e46a9115f68064572db95e6f731f143
Riyas Ahamed <56198819+riy4z@users.noreply.github.com>
2024-03-02 00:42:04 +0530

    deployment test

--- config/config.js
+++ config/config.js
@@ -1,25 +1,24 @@
 // config.js
 
 const config = {
-    database: {
-      url: 'mongodb+srv://easyroute:mffMzdtao4M2uIEg@cluster0.xdo4eov.mongodb.net/Accounts?retryWrites=true&w=majority',
-      options: {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      },
+  database: {
+    url: 'mongodb+srv://easyroute:mffMzdtao4M2uIEg@cluster0.xdo4eov.mongodb.net/Accounts?retryWrites=true&w=majority',
+    options: {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
     },
-    cors: {
-      allowedOrigins: ['http://localhost:3000', 'http://localhost:8081'],
-      optionsSuccessStatus: 200,
-    },
-    bodyParser: {
-      json: { limit: '50mb' },
-      urlencoded: { limit: '50mb', extended: true },
-    },
-    server: {
-      port: process.env.PORT || 4000,
-    },
-  };
-  
-  export default config;
-  
+  },
+  cors: {
+    allowedOrigins: ['*'], // Allow any origin
+    optionsSuccessStatus: 200,
+  },
+  bodyParser: {
+    json: { limit: '50mb' },
+    urlencoded: { limit: '50mb', extended: true },
+  },
+  server: {
+    port: process.env.PORT || 4000,
+  },
+};
+
+export default config;
